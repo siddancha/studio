603a2b2934de1e4887486b47ed50147cc7dbef91
Siddharth Ancha <sancha@cs.cmu.edu>
2022-03-23 11:43:12 -0400

    First implementation of `LightCurtainMesh`
    - Currently only rendering a fixed square.
    - TODO: use `mesh_data` to compute mesh vertices.

--- packages/studio-base/src/panels/ThreeDimensionalViz/WorldMarkers.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/WorldMarkers.tsx
@@ -31,6 +31,7 @@ import {
   OccupancyGrids,
   DynamicOccupancyGrids,
   VisibilityGrids,
+  LightCurtainMeshs,
   PointClouds,
   PoseMarkers,
   LinedConvexHulls,
@@ -80,6 +81,7 @@ export type InteractiveMarkersByType = {
   grid: Interactive<BaseMarker>[];
   dyngrid: Interactive<BaseMarker>[];
   visgrid: Interactive<BaseMarker>[];
+  lcmesh: Interactive<BaseMarker>[];
   instancedLineList: Interactive<BaseMarker>[];
   linedConvexHull: Interactive<LineListMarker | LineStripMarker>[];
   lineList: Interactive<LineListMarker>[];
@@ -168,6 +170,7 @@ export default function WorldMarkers({
     grid,
     dyngrid,
     visgrid,
+    lcmesh,
     instancedLineList,
     linedConvexHull,
     lineList,
@@ -217,6 +220,9 @@ export default function WorldMarkers({
       <VisibilityGrids layerIndex={(layerIndex as number) + LAYER_INDEX_OCCUPANCY_GRIDS}>
         {visgrid}
       </VisibilityGrids>
+      <LightCurtainMeshs layerIndex={(layerIndex as number) + LAYER_INDEX_OCCUPANCY_GRIDS}>
+        {lcmesh}
+      </LightCurtainMeshs>
       {/* Render PointClouds first so other markers with the same zIndex can show on top of PointClouds. */}
       <PointClouds layerIndex={layerIndex} clearCachedMarkers={clearCachedMarkers}>
         {pointcloud}
--- packages/studio-base/src/types/Messages.ts
+++ packages/studio-base/src/types/Messages.ts
@@ -262,6 +262,13 @@ export type DynMap$VisibilityGrid = Readonly<{
   visibility: Uint8Array;
 }>;
 
+export type LCDriver$LightCurtainMesh = Readonly<{
+  header: Header;
+  width: number;
+  height: number;
+  mesh_data: Float32Array;
+}>;
+
 export type NavMsgs$Path = Readonly<{
   header: Header;
   poses: PoseStamped[];
@@ -298,6 +305,18 @@ export type VisibilityGridMessage = Readonly<{
   info: NavMsgs$MapMetaData;
   pose: MutablePose;
   visibility: Uint8Array;
+}>;
+
+export type LightCurtainMesh = Readonly<{
+  header: Header;
+  name: string;
+  type: 113;
+  alpha?: number;
+  color?: Color;
+  pose: MutablePose;
+  width: number;
+  height: number;
+  mesh_data: Float32Array;
 }>;
 
 export type TriangleListMarker = Readonly<
